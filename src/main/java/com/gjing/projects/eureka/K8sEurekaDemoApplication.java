8a77b8bb85f8d66104ce1e0964df9e0648a9a5eb
Gjing <me@gjing.cn>
2021-06-15 18:35:14 +0800

    init

---
+++ src/main/java/com/gjing/projects/eureka/K8sEurekaDemoApplication.java
@@ -0,0 +1,18 @@
+package com.gjing.projects.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Gjing
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class K8sEurekaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sEurekaDemoApplication.class, args);
+    }
+
+}
